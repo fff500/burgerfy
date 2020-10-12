5a08317d8dc5955cb039299d5fa3db6a6cfc7752
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2020-10-12 19:31:18 +0300

    Basic project setup

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  plugins: [
+    "react"
+  ],
+  env: {
+    browser: true,
+    node: true,
+    jest: true
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      createClass: "createReactClass",
+      pragma: "React",
+      fragment: "Fragment",
+      version: "detect",
+      flowVersion: "0.53"
+    },
+    propWrapperFunctions: [
+      "forbidExtraProps",
+      {property: "freeze", object: "Object"},
+      {property: "myFavoriteWrapper"}
+    ],
+    linkComponents: [
+      "Hyperlink",
+      {name: "Link", linkAttribute: "to"}
+    ]
+  },
+  rules: {
+    "space-in-parens": ["error", "always"]
+  }
+}
